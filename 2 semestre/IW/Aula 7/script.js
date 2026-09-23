356a1060230d696648ebab8865b12f9b010c3d4f
Vernyay <willraphaelcarvalhodas@gmail.com>
2026-09-23 20:41:03 -0300

    new file:   2 semestre/IW/Aula 7/index.html 	new file:   2 semestre/IW/Aula 7/script.js

---
+++ 2 semestre/IW/Aula 7/script.js
@@ -0,0 +1,14 @@
+function aperta(){
+    var status=document.getElementById("status");
+    status.innerHTML="Você apertou o botão do mouse";
+}
+
+function solta(){
+    var status=document.getElementById("status");
+    status.innerHTML="Você soltou o botão do mouse";
+}
+
+function clica(){
+    var status2=document.getElementById("status2");
+    status2.innerHTML="Você clicou com o botão do mouse";
+}
